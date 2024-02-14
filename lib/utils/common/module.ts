80b0528da50aab9d5e85cdb863c4612ef63430d6
jaeilnet <sgsgself@naver.com>
2024-02-14 16:51:22 +0900

    fix: 패키지 모듈 타입 가져오는 버그 수정정

--- lib/utils/common/module.ts
+++ lib/utils/common/module.ts
@@ -1,5 +1,14 @@
 import { PackageModuleType } from 'lib/type';
+import { readFileSync } from 'node:fs';
 
 export const getPackageModuleType = (): PackageModuleType => {
-	return (process.env.npm_package_type as PackageModuleType) || 'commonjs';
+	if (process.env.npm_package_type) {
+		return process.env.npm_package_type as PackageModuleType;
+	}
+
+	const readFile = readFileSync(process.env.PWD + '/package.json').toString();
+	const json = JSON.parse(readFile);
+	const type = json.type as PackageModuleType;
+
+	return type;
 };
